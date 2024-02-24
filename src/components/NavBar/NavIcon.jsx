786bf4c82b32233776540d458192f0a2bef0e8df
Hossam Mousa <hossam.mousa99@yahoo.com>
2024-02-24 22:10:38 +0000

    edited links

--- src/components/NavBar/NavIcon.jsx
+++ src/components/NavBar/NavIcon.jsx
@@ -4,13 +4,13 @@ import {Link} from "react-router-dom";
 const NavIcons = () => {
   return (
     <li className="navIcons">
-      <Link to="https://www.linkedin.com/in/hossamynwa">
+      <Link to="https://www.linkedin.com/in/hossamynwa" target="_blank">
         <FaLinkedinIn />
       </Link>
-      <Link to="https://github.com/hossamynwa">
+      <Link to="https://github.com/hossamynwa" target="_blank">
         <FaGithub />
       </Link>
-      <Link to="https://www.facebook.com/HossamYNWA08">
+      <Link to="https://www.facebook.com/HossamYNWA08" target="_blank">
         <FaFacebookF />
       </Link>
     </li>
